fc9b7bc21618152d76cf1ced4bdfca3f9c8ae0e9
Noudea <oudea.nicolas@gmail.com>
2023-04-28 10:37:28 +0200

    :wastebasket: clean code and ESlint/Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+  },
+}
